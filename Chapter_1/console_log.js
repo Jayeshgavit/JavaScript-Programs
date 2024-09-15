4be44bcc5c35c444bb75626425487797d2fb68d0
Jayeshgavit <gavitjayesh08@gmail.com>
2024-09-15 20:11:02 +0530

    variable code added

---
+++ Chapter_1/console_log.js
@@ -0,0 +1,6 @@
+console.log("\n This is used to print the message to the console \n");
+
+
+name = 'Jayesh';
+age = 23;
+console.log( "Name is : ", name ,'and age is : ', age)
--- Chapter_1/first.js
+++
@@ -1,1 +0,0 @@
-console.log("JHello world")
