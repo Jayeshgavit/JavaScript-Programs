1ae9280f3c9bfd596824c1d0ca1bd92d33f09d93
Jayeshgavit <gavitjayesh08@gmail.com>
2024-09-17 23:03:51 +0530

    for loop code  added

---
+++ Chapter_3/loops.js
@@ -0,0 +1,22 @@
+console.log("for Loop Program ")
+
+
+for (var i = 1; i < 10; i++){
+    console.log(i);
+
+}
+
+
+let num = 3;
+for (var i = 1; i < 11; i++){ 
+    console.log(num +"x"+ i + ":" + num*i);
+}
+
+for (var i = 1; i < 101; i++){ 
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+
+
