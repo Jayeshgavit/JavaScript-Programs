0491edf6aa452dd9db1e11be15bc2234c0ee44a2
Jayeshgavit <gavitjayesh08@gmail.com>
2024-09-19 21:20:31 +0530

    while and do-while code added in loop.js

--- Chapter_3/loops.js
+++ Chapter_3/loops.js
@@ -23,3 +23,29 @@ for (var i = 1; i < 101; i++){
 for(var l = 1; l < 11; l++){
     document.write(l + '<br/>');
 }
+
+
+sum =0;
+for(let t= 1; t <= 5; t++){
+    console.log("Sum is ", sum ,"and t",t);
+    sum = sum + t;
+    console.log("Sum: ", sum);
+
+}
+
+document.write(" Sum of 5 number is : "+ sum)
+
+
+console.log("While loop : ")
+let count = 0;
+while(count < 5){
+    console.log(count);
+    count++;
+}
+
+var n = 1;
+var t1 = 5;
+while(n < 11){
+    console.log(n +" x "+ t1 +": "+ n*t1);
+    n++;
+}
