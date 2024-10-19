a16d8feb379499bb3623a700ee3dddebdba237e6
Jayeshgavit <gavitjayesh777@gmail.com>
2024-10-19 19:34:38 +0530

    q2 solved

--- Chapter_9/scriptps.js
+++ Chapter_9/scriptps.js
@@ -12,8 +12,30 @@ class user{
         console.log(` Name is ${this.name} and Email is ${this.email}`);
         console.log('Data is:', data, ' : ', this.name );
     };
+
+    printRoll(){
+        console.log('Roll is :');
+    }
 }
 
 let Jayesh = new user('Jayesh', 'gavitjayesh08@gmail.com');
 
 Jayesh.viewData();
+
+
+class admin extends user{
+    constructor(name, email){
+        super(name, email);
+        this.role = 'Admin';
+    }
+    editData(){
+        console.log('Name is : ',this.name, 'and Email is : ',this.email);
+        console.log('Roll is :' , this.role);
+        console.log('Editing data');
+        data = 'This is updated information about';
+        console.log('Data updated:', data);
+    }
+}
+
+let Gavit = new admin('Gavit', 'gavit@gmail.com');
+Gavit.editData();
